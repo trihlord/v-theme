700f2715a9a47448034c8602d6cf9c80369f8832
Vladimir Kubantsev <trihlord@gmail.com>
2026-08-25 16:59:39 +0300

    use vapor interop and fix dev

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, vaporInteropPlugin } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(vaporInteropPlugin).mount('#app')
